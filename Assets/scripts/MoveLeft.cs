6d7fbd3710d1a01dac0af894334ae30f589a6a6d
vincec4 <vincec4@nycstudents.net>
2025-11-24 12:09:34 -0500

    Initial commit

---
+++ Assets/scripts/MoveLeft.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+private float speed = 30;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed); 
+    }
+}
